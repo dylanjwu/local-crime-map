0d064bb81f16e74ed081a1283c8eead8321d6600
[dylanjwu] <dylanjwu@gmail.com>
2023-03-09 09:51:50 -0500

    start frontend -- map and ugly markers up; connects to BE

---
+++ frontend/src/App.js
@@ -0,0 +1,120 @@
+import './App.css'
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
+import { geocode } from './geocode';
+
+import camelCase from 'camelcase-keys';
+
+const { REACT_APP_MAPBOX_TOKEN_PUBLIC } = process.env;
+const ACCESS_TOKEN = REACT_APP_MAPBOX_TOKEN_PUBLIC;
+mapboxgl.accessToken = ACCESS_TOKEN;
+
+console.log(ACCESS_TOKEN);
+
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  const loadMap = async (data) => {
+
+    const coordinates = []
+    for (const call of data.slice(0, 10)) {
+      const {callLocation}  = call;
+      const c = await geocode(`${callLocation}, Portland, Maine`); // TODO: move this to BE
+      coordinates.push({coords: c, address: callLocation});
+    }
+
+    console.log(JSON.stringify(coordinates, null, 2))
+
+
+    // console.log(locations);
+
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+    // const marker1 = new mapboxgl.Marker()
+    //   .setLngLat([12.554729, 55.70651])
+    //   .addTo(map.current);
+
+    map.current.on('load', () => {
+      // Add an image to use as a custom marker
+      map.current.loadImage(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+        (error, image) => {
+          if (error) throw error;
+          map.current.addImage('custom-marker', image);
+          // Add a GeoJSON source with 2 points
+          map.current.addSource('points', {
+            'type': 'geojson',
+            'data': {
+              'type': 'FeatureCollection',
+              'features': coordinates.map(({coords, address}) => ({
+                  'type': 'Feature',
+                  'geometry': {
+                    'type': 'Point',
+                    'coordinates': [
+                      coords.longitude, coords.latitude
+                    ]
+                  },
+                  'properties': {
+                    'title': address
+                  }
+              }))
+            }
+          });
+
+          // Add a symbol layer
+          map.current.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'points',
+            'layout': {
+              'icon-image': 'custom-marker',
+              // get the title name from the source's "title" property
+              'text-field': ['get', 'title'],
+              'text-font': [
+                'Open Sans Semibold',
+                'Arial Unicode MS Bold'
+              ],
+              'text-offset': [0, 1.25],
+              'text-anchor': 'top'
+            }
+          });
+        }
+      );
+    });
+  }
+
+  const getCalls = async () => {
+    const res = await axios.get('http://localhost:3001/calls');
+    const { data } = res;
+    return data;
+  };
+
+  useEffect(() => {
+    getCalls().then(res => camelCase(res, { deep: true })).then((data) => loadMap(data));
+});
+
+    // useEffect(() => {
+    //   if (!map.current) return; // wait for map to initialize
+    //   map.current.on('move', () => {
+    //     setLng(map.current.getCenter().lng.toFixed(4));
+    //     setLat(map.current.getCenter().lat.toFixed(4));
+    //     setZoom(map.current.getZoom().toFixed(2));
+    //   });
+    // });
+
+    return (
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
+    );
+  }
